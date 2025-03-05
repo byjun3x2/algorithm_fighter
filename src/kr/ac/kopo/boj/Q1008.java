da3dcb7a623094630720ce0b2e9e9ef50f6e0325
byjun3x2 <byjun32@gmail.com>
2025-03-06 00:28:23 +0900

    feat: add BOJ Q1008

---
+++ src/kr/ac/kopo/boj/Q1008.java
@@ -0,0 +1,14 @@
+package kr.ac.kopo.boj;
+
+import java.util.Scanner;
+
+public class Q1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble();
+
+        System.out.println(A / B);
+    }
+}
