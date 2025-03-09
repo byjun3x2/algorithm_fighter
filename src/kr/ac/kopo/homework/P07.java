1c0e4b7b7bba4b033d5fb36083dd13cb59df3882
byjun3x2 <byjun32@gmail.com>
2025-03-09 16:57:59 +0900

    feat: 0307_homework_P07

---
+++ src/kr/ac/kopo/homework/P07.java
@@ -0,0 +1,11 @@
+package kr.ac.kopo.homework;
+
+public class P07 {
+    public static void main(String[] args) {
+        for (int i = 100; i > 0; i--) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
