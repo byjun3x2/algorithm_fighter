184eb817f58d01f671d76a357fc3740de9f1ff3c
byjun3x2 <byjun32@gmail.com>
2025-03-18 21:25:20 +0900

    feat: 250318 homework

---
+++ src/kr/ac/kopo/homework0318/StringUtil.java
@@ -0,0 +1,108 @@
+package kr.ac.kopo.homework0318;
+
+import java.util.Arrays;
+
+public class StringUtil {
+
+    /**
+     * 이 메서드는 char 자료형 매개변수를 받고 이 매개변수가 대문자이면
+     * true를 반환, 그렇지 않으면 false를 반환
+     *
+     * @param c
+     * @return true / false
+     */
+    boolean isUppercase(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 이 메서드는 char 자료형 매개변수를 받고 이 매개변수가 소문자이면
+     * true를 반환, 그렇지 않으면 false를 반환
+     *
+     * @param c
+     * @return true / false
+     */
+    boolean isLowercase(char c) {
+        if (c >= 'a' && c <= 'z') { // 소문자 범위 확인
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 이 메서드는 int 자료형 매개변수 i , j 를 받고
+     * 더 큰 수를 반환합니다.
+     *
+     * @param i
+     * @param j
+     * @return true / false
+     */
+    int max(int i, int j) {
+        return Math.max(i, j);
+    }
+
+    /**
+     * 이 메서드는 int 자료형 매개변수 i , j 를 받고
+     * 더 작은 수를 반환합니다.
+     *
+     * @param i
+     * @param j
+     * @return true / false
+     */
+    int min(int i, int j) {
+        return Math.min(i, j);
+    }
+
+    String reverseString(String str) {
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+        return "변환이 완료되었습니다.";
+    }
+
+    // 1번줄에 StringBuffer 인스턴스 생성
+    String toUpperString(String str) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            // 소문자인 경우 대문자로 변환하여 추가
+            if (ch >= 'a' && ch <= 'z') {
+                sb.append((char) (ch - 32)); // ASCII 값을 이용해 대문자로 변환
+            } else {
+                sb.append(ch); // 소문자가 아니면 그대로 추가
+            }
+        }
+        return sb + "";
+    }
+
+    String toLowerString(String str) {
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            // 소문자인 경우 대문자로 변환하여 추가
+            if (ch >= 'a' && ch <= 'z') {
+                sb.append((char) (ch - 32)); // ASCII 값을 이용해 대문자로 변환
+            } else {
+                sb.append(ch); // 소문자가 아니면 그대로 추가
+            }
+        }
+        return sb.toString();
+    }
+
+    public static int checkChar (String strData,char ch){
+        int count = 0;
+        String[] arr = strData.split("");
+//        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(String.valueOf(ch))){
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/kr/ac/kopo/homework0318/StringUtilMain.java
@@ -0,0 +1,45 @@
+package kr.ac.kopo.homework0318;
+
+import java.util.Scanner;
+
+public class StringUtilMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("하나의 문자를 입력해 주세요 : ");
+
+        char ch = sc.nextLine().charAt(0); // 2. 받은 값을 char로 변환
+        StringUtil su = new StringUtil(); // StringUtil 인스턴스 생성
+
+        // 문제 1, 2
+        // Character.isUpperCase() 정적 메서드
+        //  매개변수에 대문자가 들어가면 true를 리턴 , 소문자가 들어가면 false를 리턴
+        if (Character.isUpperCase(ch)) {
+            System.out.println("입력한 문자는 대문자입니다. isLowerCase() 메서드를 실행합니다.");
+            System.out.println("이 함수는 " + su.isUppercase(ch) + "를 반환합니다.");
+        } else {
+            System.out.println("입력한 문자는 소문자입니다. isUpperCase() 메서드를 실행합니다.");
+            System.out.println("이 함수는 " + su.isLowercase(ch) + "를 반환합니다.");
+        }
+
+        // 문제 3, 4
+        Scanner sc2 = new Scanner(System.in);
+        System.out.print("숫자 두개를 입력해 주세요 : \t");
+        int a = sc2.nextInt();
+        int b = sc2.nextInt();
+        System.out.println("입력한 숫자는 " + a + " 와 " + b + " 입니다.");
+        System.out.println("큰 수는 " + su.max(a, b) + " 작은 수는 " + su.min(a, b) + "입니다.");
+
+        // 문제 5
+        su.reverseString("Apple");
+        System.out.println();
+        // 문제 6
+        su.toUpperString("galaxy");
+        // 문제 7
+        su.toLowerString("PSYCHO");
+        // 문제 8
+
+        // 문제 9
+        int count = su.checkChar("hello", 'l');
+        System.out.println(count);
+    }
+}
