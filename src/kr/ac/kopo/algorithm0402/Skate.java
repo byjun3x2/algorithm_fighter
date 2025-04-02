0098c02898f741c37a4eb07bc92e7799f56da017
byjun3x2 <byjun32@gmail.com>
2025-04-02 21:31:01 +0900

    250402_Algorithm

---
+++ src/kr/ac/kopo/algorithm0402/Skate.java
@@ -0,0 +1,18 @@
+package kr.ac.kopo.algorithm0402;
+
+import java.util.Scanner;
+
+public class Skate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 7; j++) {
+                
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/kr/ac/kopo/algorithm0402/VERA.java
@@ -0,0 +1,14 @@
+package kr.ac.kopo.algorithm0402;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println(N * (N-1));
+
+    }
+}
